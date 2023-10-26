405b2001eee06e31631833d6189d0ccbf02bc6e1
YC.W <contact@wangyuchen.cn>
2023-10-26 18:31:45 +0800

    feat: examples

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,38 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { DefaultContentFormatter, EasyLogger } from '@yc-w-cn/nest-easy-logger';
 
 @Controller()
 export class AppController {
+  private logger = new EasyLogger(AppController.name);
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  BasicUse(): string {
+    // Output Key
+    this.logger.log('hi');
+
+    // Output Key + Value
+    this.logger.log('hi', 'hello world');
+
+    // Output Key + Object
+    this.logger.log('hi', { text: 'hello world' });
+
+    return this.appService.getHello();
+  }
+
+  @Get('custom')
+  CustomUse(): string {
+    const customLogger = new EasyLogger(AppController.name);
+    customLogger.setContentFormatter(
+      new DefaultContentFormatter({
+        relaceEmptyValue: true,
+      }),
+    );
+
+    // Output Replace Empty Value
+    customLogger.log('', '');
+
     return this.appService.getHello();
   }
 }
